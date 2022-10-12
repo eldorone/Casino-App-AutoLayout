40e1e4ac3d6c775710a5b4fdee8773d72a61736d
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-12 23:20:28 +0900

    Update ViewController.swift

--- autolayout-practice/ViewController.swift
+++ autolayout-practice/ViewController.swift
@@ -21,8 +21,8 @@ class ViewController: UIViewController {
                          UIImage(named: "DiceFive"),
                          UIImage(named: "DiceSix")]
                 
-        diceImageView1.image = diceArr[Int.random(in: 0 ... 5)]
-        diceImageView2.image = diceArr[Int.random(in: 0 ... 5)]
+        diceImageViewOne.image = diceArr[Int.random(in: 0 ... 5)]
+        diceImageViewTwo.image = diceArr[Int.random(in: 0 ... 5)]
         
     }
     
